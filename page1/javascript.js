81c8773045cfa44d573f6995c93482563b3a488d
BThanatonz <super.b99@hotmail.com>
2024-06-28 12:54:17 +0700

    add json data to card

--- page1/javascript.js
+++ page1/javascript.js
@@ -6,28 +6,44 @@ function fetchJSON(url) {
     });
 }
 
-(async () => {
-  var json = await fetchJSON("https://dummyjson.com/products");
-  console.log(json.products);
-})();
+// (async () => {
+//   var json = await fetchJSON(
+//     "https://www.themealdb.com/api/json/v1/1/random.php"
+//   );
+//   console.log(json.meals[0]);
+// })();
 
+// json.forEach((element) => {
+//   document.getElementById(
+//     "show_products"
+//   ).innerHTML += ` <div class="card" style="width: 18rem">
+//                       <img src=".." class="card-img-top" alt="..." />
+//                       <div class="card-body">
+//                         <h5 class="card-title">Card title</h5>
+//                         <p class="card-text">
+//                           Some quick example text to build on the card title and make up the
+//                           bulk of the card's content.
+//                         </p>
+//                         <a href="#" class="btn btn-primary">Go somewhere</a>
+//                       </div>
+//                     </div>`;
+// });
 
-
-
-function initProduct(Json_data){
-    json.products.forEach(element => {
-        document.getElementById(
-            "show_products"
-          ).innerHTML += ` <div class="card" style="width: 18rem">
-                      <img src="${element}" class="card-img-top" alt="..." />
-                      <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">
-                          Some quick example text to build on the card title and make up the
-                          bulk of the card's content.
-                        </p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                      </div>
-                    </div>`;
-    });
+for (let i = 0; i < 6; i++) {
+  (async () => {
+    var json = await fetchJSON(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    console.log(json.meals[0]);
+    document.getElementById(
+        "show_products"
+      ).innerHTML += ` <div class="card" style="width: 18rem">
+                          <img src="${json.meals[0].strMealThumb}" class="w-100 card-img-top" alt="..." />
+                          <div class="card-body">
+                            <h5 class="card-title">${json.meals[0].strMeal}</h5>
+                            <a href="#" class="btn btn-primary">Detail</a>
+                          </div>
+                        </div>`;
+  })();
+ 
 }
